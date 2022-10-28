bca1cfe923c60f98aed1eb11dad275d365398021
200573 <200573@E0505-37>
2022-10-28 15:31:35 +0900

    Sentiment.javaが動いた

--- src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Json05.java
@@ -21,10 +21,9 @@ public class Json05 {
 	static Language getLanguage(String s) throws IOException, URISyntaxException, InterruptedException {
 		Gson gson = new Gson();
 
-		String url = "https://r04jk3ab-text.cognitiveservices.azure.com//" + "text/analytics/v3.0/languages";
+		String url = "https://r04jk3a14-text.cognitiveservices.azure.com/" + "text/analytics/v3.0/languages";
 		Map<String, String> map = new HashMap<>();
-		map.put("Ocp-Apim-Subscription-Key", "953ea066e1384085b69e5bd5cb3c20d0");
-
+		map.put("Ocp-Apim-Subscription-Key", "5dd287f97bfa4609ab527ee317b583de");
 		Docs doc = new Docs();
 		doc.id = "1";
 		doc.text = s;
---
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/Sentiment.java
@@ -0,0 +1,75 @@
+package jp.jc21.t.yoshizawa.WEB01;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+public class Sentiment {
+
+	public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+		Language1 message = getSentiment("子どもたちがいなかったので、 鳥たちは歌いたいと思いませんでしたし、 木々は花を咲かせるのを忘れておりました。");
+		if (message != null) {
+			System.out.println(message.documents[0].confidenceScores.positive);
+			System.out.println(message.documents[0].confidenceScores.neutral);
+			System.out.println(message.documents[0].confidenceScores.negative);
+		}
+	}
+
+	static Language1 getSentiment(String s) throws IOException, URISyntaxException, InterruptedException {
+		Gson gson = new Gson();
+
+		String url = "https://r04jk3a14-text.cognitiveservices.azure.com/" + "text/analytics/v3.0/sentiment";
+		Map<String, String> map = new HashMap<>();
+		map.put("Ocp-Apim-Subscription-Key", "5dd287f97bfa4609ab527ee317b583de");
+		Docs2 doc = new Docs2();
+		doc.id = "1";
+		doc.text = s;
+
+		Source1 src = new Source1();
+		src.documents = new Docs2[1];
+		src.documents[0] = doc;
+
+		String jsonData = new Gson().toJson(src);
+
+		InetSocketAddress proxy = new InetSocketAddress("172.17.0.2", 80);
+
+		JsonReader reader = WebApiConnector.postJsonReader(url, proxy, map, jsonData);
+		Language1 message = null;
+		if (reader != null) {
+			message = gson.fromJson(reader, Language1.class);
+			reader.close();
+		}
+		return message;
+	}
+
+}
+
+class Language1 {
+	Documents1[] documents;
+	String[] errors;
+	String modelVersion;
+}
+
+class Documents1 {
+	ConfidenceScores confidenceScores;
+}
+
+class ConfidenceScores {
+	double negative;
+	double neutral;
+	double positive;
+}
+
+class Source1 {
+	Docs2[] documents;
+}
+
+class Docs2 {
+	String id;
+	String text;
+}
